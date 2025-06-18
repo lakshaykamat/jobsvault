0ec55bf1a40be910a75e517b5de6a58b80df0b24
Lakshay Kamat <lakshaykamat2048@gmail.com>
2025-06-18 23:49:03 +0530

    fix: update CORS origin to include CLIENT_URL from environment variables

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
+    origin: [
+      process.env.CLIENT_URL,
+      "http://127.0.0.1:3000",
+      "http://localhost:3000",
+    ],
     credentials: true,
   })
 );
